646633f14eb90492edb05471f24ab788dc80bb02
miho19 <miho19@hotmail.co.nz>
2025-10-12 09:00:29 +1300

    stubs for user controller

---
+++ gymapi/Controllers/User.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace gymapi.Controllers;
+
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Authorize]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> _logger;
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("GetMe")]
+    public Task<IActionResult> GetMe()
+    {
+
+    }
+}
